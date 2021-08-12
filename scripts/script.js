c758cb1614c36db24b65dd2d00fe659550019a4a
Yogapriya P <yogapriya.pappaian@solitontech.com>
2021-08-12 15:37:55 +0530

    fix : Default city and its values are displayed on page load
    Signed-off-by: Yogapriya P <yogapriya.pappaian@solitontech.com>

--- scripts/script.js
+++ scripts/script.js
@@ -1,205 +1,4 @@
 import { mydata } from './data.js';
-console.log(mydata);
-// let mydata = {
-//   nome: {
-//     cityName: 'Nome',
-//     dateAndTime: '3/31/2020, 9:21:46 PM',
-//     timeZone: 'America/Nome',
-//     temperature: '4°C',
-//     humidity: '91%',
-//     precipitation: '8%',
-//     nextFiveHrs: ['6°C', '7°C', '11°C', '2°C'],
-//   },
-//   newyork: {
-//     cityName: 'NewYork',
-//     dateAndTime: '4/1/2020, 1:21:46 AM',
-//     timeZone: 'America/New_york',
-//     temperature: '23°C',
-//     humidity: '52%',
-//     precipitation: '46%',
-//     nextFiveHrs: ['18°C', '20°C', '18°C', '17°C'],
-//   },
-//   jamaica: {
-//     cityName: 'Jamaica',
-//     dateAndTime: '4/1/2020, 12:21:46 AM',
-//     timeZone: 'America/Jamaica',
-//     temperature: '29°C',
-//     humidity: '39%',
-//     precipitation: '58%',
-//     nextFiveHrs: ['24°C', '27°C', '28°C', '24°C'],
-//   },
-//   losangeles: {
-//     cityName: 'LosAngeles',
-//     dateAndTime: '3/31/2020, 10:21:46 PM',
-//     timeZone: 'America/Los_Angeles',
-//     temperature: '23°C',
-//     humidity: '52%',
-//     precipitation: '46%',
-//     nextFiveHrs: ['19°C', '21°C', '23°C', '18°C'],
-//   },
-//   winnipeg: {
-//     cityName: 'Winnipeg',
-//     dateAndTime: '4/1/2020, 12:21:46 AM',
-//     timeZone: 'America/Winnipeg',
-//     temperature: '9°C',
-//     humidity: '81%',
-//     precipitation: '18%',
-//     nextFiveHrs: ['3°C', '7°C', '4°C', '-1°C'],
-//   },
-//   juba: {
-//     cityName: 'Juba',
-//     dateAndTime: '4/1/2020, 8:21:46 AM',
-//     timeZone: 'Africa/Juba',
-//     temperature: '18°C',
-//     humidity: '62%',
-//     precipitation: '36%',
-//     nextFiveHrs: ['37°C', '41°C', '38°C', '37°C'],
-//   },
-//   maseru: {
-//     cityName: 'Maseru',
-//     dateAndTime: '4/1/2020, 7:21:46 AM',
-//     timeZone: 'Africa/Maseru',
-//     temperature: '43°C',
-//     humidity: '10%',
-//     precipitation: '86%',
-//     nextFiveHrs: ['17°C', '17°C', '19°C', '15°C'],
-//   },
-//   london: {
-//     cityName: 'London',
-//     dateAndTime: '4/1/2020, 6:21:46 AM',
-//     timeZone: 'Europe/London',
-//     temperature: '7°C',
-//     humidity: '85%',
-//     precipitation: '14%',
-//     nextFiveHrs: ['-7°C', '-6°C', '-5°C', '-10°C'],
-//   },
-//   vienna: {
-//     cityName: 'Vienna',
-//     dateAndTime: '4/1/2020, 7:21:46 AM',
-//     timeZone: 'Europe/Vienna',
-//     temperature: '10°C',
-//     humidity: '79%',
-//     precipitation: '20%',
-//     nextFiveHrs: ['-1°C', '-1°C', '4°C', '-2°C'],
-//   },
-//   moscow: {
-//     cityName: 'Moscow',
-//     dateAndTime: '4/1/2020, 8:21:46 AM',
-//     timeZone: 'Europe/Moscow',
-//     temperature: '11°C',
-//     humidity: '77%',
-//     precipitation: '22%',
-//     nextFiveHrs: ['12°C', '14°C', '15°C', '8°C'],
-//   },
-//   dublin: {
-//     cityName: 'Dublin',
-//     dateAndTime: '4/1/2020, 6:21:46 AM',
-//     timeZone: 'Europe/Dublin',
-//     temperature: '15°C',
-//     humidity: '68%',
-//     precipitation: '30%',
-//     nextFiveHrs: ['24°C', '28°C', '29°C', '21°C'],
-//   },
-//   karachi: {
-//     cityName: 'Karachi',
-//     dateAndTime: '4/1/2020, 10:21:46 AM',
-//     timeZone: 'Asia/Karachi',
-//     temperature: '25°C',
-//     humidity: '47%',
-//     precipitation: '50%',
-//     nextFiveHrs: ['21°C', '24°C', '21°C', '19°C'],
-//   },
-//   kolkata: {
-//     cityName: 'Kolkata',
-//     dateAndTime: '4/1/2020, 10:51:46 AM',
-//     timeZone: 'Asia/Kolkata',
-//     temperature: '34°C',
-//     humidity: '29%',
-//     precipitation: '68%',
-//     nextFiveHrs: ['33°C', '35°C', '37°C', '32°C'],
-//   },
-//   yangon: {
-//     cityName: 'Yangon',
-//     dateAndTime: '4/1/2020, 11:51:46 AM',
-//     timeZone: 'Asia/Yangon',
-//     temperature: '24°C',
-//     humidity: '50%',
-//     precipitation: '48%',
-//     nextFiveHrs: ['31°C', '32°C', '36°C', '27°C'],
-//   },
-//   bangkok: {
-//     cityName: 'BangKok',
-//     dateAndTime: '4/1/2020, 12:21:46 PM',
-//     timeZone: 'Asia/BangKok',
-//     temperature: '32°C',
-//     humidity: '33%',
-//     precipitation: '64%',
-//     nextFiveHrs: ['32°C', '34°C', '32°C', '32°C'],
-//   },
-//   seoul: {
-//     cityName: 'Seoul',
-//     dateAndTime: '4/1/2020, 2:21:46 PM',
-//     timeZone: 'Asia/Seoul',
-//     temperature: '6°C',
-//     humidity: '87%',
-//     precipitation: '12%',
-//     nextFiveHrs: ['1°C', '1°C', '3°C', '0°C'],
-//   },
-//   anadyr: {
-//     cityName: 'Anadyr',
-//     dateAndTime: '4/1/2020, 5:21:46 PM',
-//     timeZone: 'Asia/Anadyr',
-//     temperature: '-2°C',
-//     humidity: '100%',
-//     precipitation: '0%',
-//     nextFiveHrs: ['-1°C', '3°C', '-1°C', '-3°C'],
-//   },
-//   brokenhill: {
-//     cityName: 'BrokenHill',
-//     dateAndTime: '4/1/2020, 3:51:46 PM',
-//     timeZone: 'Australia/Broken_Hill',
-//     temperature: '10°C',
-//     humidity: '79%',
-//     precipitation: '20%',
-//     nextFiveHrs: ['17°C', '21°C', '22°C', '15°C'],
-//   },
-//   perth: {
-//     cityName: 'Perth',
-//     dateAndTime: '4/1/2020, 1:21:46 PM',
-//     timeZone: 'Australia/Perth',
-//     temperature: '18°C',
-//     humidity: '62%',
-//     precipitation: '36%',
-//     nextFiveHrs: ['6°C', '10°C', '11°C', '6°C'],
-//   },
-//   auckland: {
-//     cityName: 'Auckland',
-//     dateAndTime: '4/1/2020, 6:21:46 PM',
-//     timeZone: 'Pacific/Auckland',
-//     temperature: '17°C',
-//     humidity: '64%',
-//     precipitation: '34%',
-//     nextFiveHrs: ['17°C', '19°C', '18°C', '17°C'],
-//   },
-//   vostok: {
-//     cityName: 'Vostok',
-//     dateAndTime: '4/1/2020, 11:21:46 AM',
-//     timeZone: 'Antarctica/Vostok',
-//     temperature: '-61°C',
-//     humidity: '100%',
-//     precipitation: '0%',
-//     nextFiveHrs: ['-64°C', '-59°C', '-64°C', '-68°C'],
-//   },
-//   troll: {
-//     cityName: 'Troll',
-//     dateAndTime: '4/1/2020, 7:21:46 AM',
-//     timeZone: 'Antarctica/Troll',
-//     temperature: '-52°C',
-//     humidity: '100%',
-//     precipitation: '0%',
-//     nextFiveHrs: ['-70°C', '-70°C', '-66°C', '-71°C'],
-//   },
-// };
 
 //..................................Event to populate the city names into the dropdown........................//
 var city_key = [];
@@ -218,6 +17,24 @@ let city_list = function PopulateDropDownList() {
 window.addEventListener('load', city_list);
 
 //....................................Functions to change values of the top section...........................//
+window.addEventListener('load', defaultValue);
+function defaultValue() {
+  let default_city = city_key[0];
+  ChangeDate(default_city);
+  ChangeTime(default_city);
+  ChangeTemp(default_city);
+  ChangeTimeline(default_city);
+  ChangeIcon(default_city);
+}
+// function defaultValue(anadyr)
+// {
+//   ChangeDate(anadyr);
+//   ChangeTime(anadyr);
+//   ChangeTemp(anadyr);
+//   ChangeTimeline(anadyr);
+//   ChangeIcon(anadyr);
+// }
+
 document.getElementById('city_input').addEventListener('change', ChangeValues);
 function ChangeValues() {
   let flag = 0;
@@ -257,7 +74,7 @@ function ChangeValues() {
     ChangeDate(val);
     ChangeTime(val);
     ChangeTemp(val);
-    // ChangeTimeline(val);
+    ChangeTimeline(val);
     ChangeIcon(val);
   }
 }
@@ -453,20 +270,13 @@ function ChangeWeatherIcon(temp, id) {
 
 //.............................................Middle Section..............................................//
 
-let nav_items = document.getElementsByClassName('navbar-item');
-function activeItem() {
-  let current = document.getElementsByClassName('active');
-  current[0].className = current[0].className.replace(' active', '');
-  this.className += ' active';
-}
-
 //...........................................................Left and Right Scroll.......................................//
 
 window.addEventListener('load', middleSection);
 
-function middleSection() {
+function middleSection(val) {
   LeftandRightScroll();
-  changeActiveState();
+  changeActiveState(val);
 }
 
 function LeftandRightScroll() {
@@ -495,6 +305,23 @@ function changeActiveState() {
   icon_snowy.addEventListener('click', activeSnowy);
   icon_rainy.addEventListener('click', activeRainy);
 
+  let temp = [];
+  let temp_sunny = [];
+  let temp_snowy = [];
+  let temp_rainy = [];
+  for (let i in mydata) {
+    temp.push(mydata[i].temperature.split('°')[0]);
+  }
+  // console.log(temp_key[0]);
+  // for(let i in temp)
+  // {
+  //   if(temp[i]>29)
+  //   {
+  //     temp_sunny.push(temp[i]);
+  //   }
+  //   else if(temp[i]<=28)
+  // }
+
   function activeSunny() {
     icon_sunny.setAttribute('class', 'navbar-item active');
     icon_rainy.setAttribute('class', 'navbar-item');
@@ -511,3 +338,7 @@ function changeActiveState() {
     icon_snowy.setAttribute('class', 'navbar-item');
   }
 }
+
+//...................................Function to display the cards based on the input value.....//
+
+function DisplayTopCities() {}
